96eb40447325c4884d4ed22377f7c8efd64248ee
stephenp0320 <stephen power>
2025-12-01 16:08:10 +0000

    Authentication lab - exercise completed

--- tasky-api/api/tasks/users/index.js
+++ tasky-api/api/tasks/users/index.js
@@ -31,7 +31,15 @@ router.post('/', asyncHandler(async (req, res) => {
 
 
 async function registerUser(req, res) {
-    // Add input validation logic here
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+    if (!regex.test(req.body.password)) {
+        return res.status(400).json({
+            success: false,
+            msg: 'Weak password. Must be 8+ chars, include a letter, a number, and a special character.'
+        });
+    }
+
     await User.create(req.body);
     res.status(201).json({ success: true, msg: 'User successfully created.' });
 }
